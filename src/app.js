34f8cb2f27f1c4c8b8a9cbd2b8a6e32f00c1dfd5
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-11-18 20:04:41 +0300

    feat: setup server up and running

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+// const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// // user routes
+// app.use("/", userRoutes);
+
+const http = require("http");
+const server = http.createServer(app);
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () =>
+  console.log(`server started and running on port ${PORT}...`)
+);
+
+module.exports = app;
