6e7f02b1ad242d3861dc10cfdcb33d236ba3d482
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-11-19 00:09:29 +0300

    feat: render html files in views directory

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/views"));
 
 // // user routes
 // app.use("/", userRoutes);
