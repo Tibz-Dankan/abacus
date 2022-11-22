f7f2de067994e94d8a44b203c0a6274178d4f0ca
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-11-22 10:39:16 +0300

    feat(token verification): validating jwt tokens

---
+++ src/utils/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (token, req, res, next) => {
+  jwt.verify(token, process.env.JWT_SECRETE_TOKEN, (error, userId) => {
+    res.clearCookie("token");
+    if (error) return res.redirect("signin");
+    req.id = userId;
+    next();
+  });
+};
+
+const verifyToken = (req, res, next) => {
+  try {
+    // TODO: implement signed cookie
+    //console.log(req.signedCookies);
+    //const token = req.signedCookies["token"];
+
+    if (!req.cookies) return res.redirect("signin");
+    const token = req.cookies.token;
+    if (!token) return res.redirect("signin");
+
+    verifyJWT(token, req, res, next);
+  } catch (error) {
+    console.log("Jwt catch error: " + error.message);
+    console.log(error);
+  }
+};
+
+module.exports = { verifyToken };
