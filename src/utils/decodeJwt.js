9e5c05e9811edb3f2ec64d65563e2fcd2d36176f
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-11-25 03:10:18 +0300

    feat(decodeb username): decode jwt to get username

--- src/utils/decodeJwt.js
+++ src/utils/decodeJwt.js
@@ -3,25 +3,15 @@ const jwt_decode = require("jwt-decode");
 const decodeJwtGetUserId = (requestCookie) => {
   const jwtToken = requestCookie.token;
   const decodedToken = jwt_decode(jwtToken);
-  console.log("Decoded token");
-  console.log(decodedToken);
   const userId = decodedToken.userId;
-  console.log("userId", userId);
   return userId;
 };
 
-module.exports = { decodeJwtGetUserId };
+const decodeJwtGetUserName = (requestCookie) => {
+  const jwtToken = requestCookie.token;
+  const decodedToken = jwt_decode(jwtToken);
+  const userName = decodedToken.userName;
+  return userName;
+};
 
-//Example from npm
-// import jwt_decode from "jwt-decode";
-
-// var token = "eyJ0eXAiO.../// jwt token";
-// var decoded = jwt_decode(token);
-
-// console.log(decoded);
-
-// /* prints:
-//  * { foo: "bar",
-//  *   exp: 1393286893,
-//  *   iat: 1393268893  }
-//  */
+module.exports = { decodeJwtGetUserId, decodeJwtGetUserName };
